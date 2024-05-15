2b8e07b70374976e0b8022afc83e80fc57b1a145
feehaam <mdfeeham@gmail.com>
2024-05-15 13:21:35 +0600

    JPA basic init

---
+++ Jpa_H2_Basic/src/main/java/com/feeham/jpa_h2_Basic/JpaH2BasicApplication.java
@@ -0,0 +1,13 @@
+package com.feeham.jpa_h2_Basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2BasicApplication.class, args);
+	}
+
+}
---
+++ Jpa_H2_Basic/src/test/java/com/feeham/jpa_h2_Basic/JpaH2BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feeham.jpa_h2_Basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
