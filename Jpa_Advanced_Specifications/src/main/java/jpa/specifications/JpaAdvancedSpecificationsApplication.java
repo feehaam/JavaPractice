89c6c6c18d3a359b77cc18fd3df09e20163faba8
feehaam <mdfeeham@gmail.com>
2024-05-19 13:09:22 +0600

    Jpa_Advanced_Specifications: Initialized...

---
+++ Jpa_Advanced_Specifications/src/main/java/jpa/specifications/JpaAdvancedSpecificationsApplication.java
@@ -0,0 +1,13 @@
+package jpa.specifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancedSpecificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancedSpecificationsApplication.class, args);
+	}
+
+}
---
+++ Jpa_Advanced_Specifications/src/test/java/jpa/specifications/JpaAdvancedSpecificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.specifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancedSpecificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
